881fe0629fb4989936a6b8ea05dabdebd588b750
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-02-11 02:48:18 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,11 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Rudra Sankha Sinhamahapatra | Full Stack Developer",
+  metadataBase: new URL("https://rudrasankha.com"),
+  title: {
+    default: "Rudra Sankha Sinhamahapatra",
+    template: "%s | Full Stack Developer",
+  },
   description:
     "I'm a Full Stack Developer skilled in React, Next.js, TypeScript,AWS, Node js. Check out my portfolio!",
   openGraph: {
@@ -27,13 +31,14 @@ export const metadata: Metadata = {
     description:
       "Explore my projects, blog, and experience in full stack development,web3,devops",
     url: "https://rudrasankha.com",
-    siteName: "Your Portfolio",
+    siteName: "Rudra Sankha Portfolio",
+    locale: "en_US",
     images: [
       {
         url: "https://avatars.githubusercontent.com/u/101992909?v=4",
         width: 1200,
         height: 630,
-        alt: "Rudra Sankha",
+        alt: "Rudra Sankha Sinhamahapatra",
       },
     ],
     type: "website",
@@ -44,6 +49,17 @@ export const metadata: Metadata = {
     description: "Explore my projects and fullstack development journey",
     site: "@RudraSankha",
     images: ["https://avatars.githubusercontent.com/u/101992909?v=4"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
 };
 
