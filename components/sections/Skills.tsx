687f2bc69781e8910c0a5670215c8204556f8126
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-02-14 21:44:13 +0530

    improved about section and skills sectionw with logos

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -1,19 +1,27 @@
-import { skills } from "@/lib/skills"
+/* eslint-disable @next/next/no-img-element */
+import { skills } from "@/lib/skills";
 
 export const Skills = () => {
-    return(
-        <main>
-             <section id="skills" className="min-h-screen py-20">
-          <h2 className="mb-12 text-center text-3xl font-bold">Skills</h2>
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {skills.map((skill) => (
-              <div key={skill} className="rounded-lg border border-[#D14309] bg-card p-6 text-card-foreground shadow-sm">
-                <h3 className="text-xl font-semibold">{skill}</h3>
-              </div>
-            ))}
-          </div>
-        </section>
-
-        </main>
-    )
-}
+  return (
+    <main>
+      <section id="skills" className="min-h-screen py-20">
+        <h2 className="mb-12 text-center text-3xl font-bold">Skills</h2>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {skills.map((skill) => (
+            <div
+              key={skill.name}
+              className="flex items-center rounded-lg border border-[#D14309] bg-card p-6 text-card-foreground shadow-sm"
+            >
+              <img
+                src={skill.icon || ""}
+                alt={`${skill.name} icon`}
+                className={`w-${skill.width} h-${skill.height} mr-4`}
+              />
+              <h3 className="text-xl font-semibold">{skill.name}</h3>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
