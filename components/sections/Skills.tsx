dd87e2d38133ab34ab265b8950f5bc36082d1410
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-05-31 23:31:51 +0530

    skills border size

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -6,16 +6,16 @@ export const Skills = () => {
     <main>
       <section id="skills" className="min-h-screen pt-20">
         <h2 className="mb-12 text-center text-3xl font-bold">Skills</h2>
-        <div className="max-w-4xl mx-auto grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4">
+        <div className="max-w-4xl max-xs2:max-w-[500px] max-xs:max-w-[200px] max-xs2:min-w-[300px] mx-auto grid grid-cols-1 xs:grid-cols-2 xs2:grid-cols-3 sm:grid-cols-4 lg:grid-cols-5  gap-3 px-4">
           {skills.map((skill) => (
             <div
               key={skill.name}
-              className="flex items-center rounded-lg border border-[#D14309] bg-card p-3 text-card-foreground shadow-sm"
+              className="flex items-center rounded-lg border border-[#D14309] bg-card p-2.5 text-card-foreground shadow-sm"
             >
               <img
                 src={skill.icon || ""}
                 alt={`${skill.name} icon`}
-                className={`w-${skill.width} h-${skill.height} mr-4`}
+                className={`w-${skill.width} h-${skill.height} mr-3`}
               />
               <h3 className="text-sm font-semibold">{skill.name}</h3>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'xs': '375px',
+      'xs2': '500px',
+      'sm': '640px',
+      'msm': '768px',  // medium-small (between small and medium)
+      'md': '960px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         background: "var(--background)",
