195eda26e8030593a128568f9bd996d16db747d0
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2026-01-13 04:39:36 +0530

    feat: adds caching to contributions data

--- app/api/github-contributions/route.ts
+++ app/api/github-contributions/route.ts
@@ -2,19 +2,45 @@ import { fetchGitHubContributions } from "@/lib/githubContributions";
 import { checkRateLimit } from "@/lib/rate-limit";
 import { NextRequest, NextResponse } from "next/server";
 
+let cachedData: any = null;
+let lastFetchTime = 0;
+
+const CACHE_TTL = 10 * 60 * 1000; 
+
 export async function POST(req: NextRequest) {
     try {
-        const { rateLimited } = await checkRateLimit(req);
+
+        const { rateLimited } = checkRateLimit(req);
 
         if (rateLimited) {
-            return NextResponse.json({ error: 'Rate limit exceeded' }, { status: 429 })
+            return NextResponse.json(
+                { error: 'Rate limit exceeded' }, 
+                { status: 429 }
+            )
         }
+
+        const now = Date.now();
+
+        if (cachedData && now - lastFetchTime < CACHE_TTL) {
+            console.log("Returning cached GitHub contributions data");
+            return NextResponse.json(cachedData, {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+        }
+
         let { from, to } = await req.json();
 
         from = new Date(from);
         to = new Date(to);
 
         const result = await fetchGitHubContributions('Rudra-Sankha-Sinhamahapatra', from, to);
+        console.log("Fetched new GitHub contributions api data");
+
+        cachedData = result;
+        lastFetchTime = now;
 
         return NextResponse.json(result, {
             status: 200,
