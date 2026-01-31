f79ed5d259a400be94a321998b6f07838a4d1529
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2026-01-31 15:30:22 +0530

    perf(seo): update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,16 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    { url: "https://rudrasankha.com", lastModified: new Date() },
-    { url: "https://cool.rudrasankha.com", lastModified: new Date() },
+    { url: "https://rudrasankha.com", lastModified: new Date(), changeFrequency: 'daily', priority: 0.8 },
+    { url: "https://rudrasankha.com/Resume.pdf", lastModified: new Date(), changeFrequency: 'daily', priority: 0.8 },
+    { url: "https://rudrasankha.com/projects", lastModified: new Date(), changeFrequency: 'daily', priority: 0.8 },
+    { url: "https://rudrasankha.com/projects/1", lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
+    { url: "https://rudrasankha.com/projects/2", lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
+    { url: "https://rudrasankha.com/projects/3", lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
+    { url: "https://rudrasankha.com/projects/4", lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
+    { url: "https://rudrasankha.com/projects/5", lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
+    { url: "https://rudrasankha.com/projects/6", lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
+    { url: "https://rudrasankha.com/projects/7", lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
+    { url: "https://rudrasankha.com/projects/8", lastModified: new Date(), changeFrequency: 'weekly', priority: 0.7 },
   ];
 }
